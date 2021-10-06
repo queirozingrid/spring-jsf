6660d4b58be6033984b6b1c3477337891d9d86a3
Ingrid Queiroz <ingrid.queiroz@boavista.local>
2021-10-06 17:50:59 -0300

    primeiro commit

---
+++ src/main/java/domain/Main.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class Main {
+	public static void main(String[] args) {
+		Usuario usuario = new Usuario();
+		usuario.setNome("ZÈZin");
+		
+		System.out.println(usuario.getNome());
+	}
+
+}
